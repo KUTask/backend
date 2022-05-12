47b38809075880fcebc37d55705ad42899973473
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-05-12 14:46:13 +0700

    Remove semi

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const port = process.env.PORT ?? 4000;
+  const port = process.env.PORT ?? 4000
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
-  );
-  await app.listen(port, '0.0.0.0');
+  )
+  await app.listen(port, '0.0.0.0')
 }
-bootstrap();
+bootstrap()
