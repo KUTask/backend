e5d5d79a0f6a719d8c720abb60eb2d47651d04c7
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-05-16 13:34:04 +0700

    Inject module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GqlConfigService } from './configs/gql.config'
 import { HealthModule } from './health/health.module'
+import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
+import configuration from './configs'
 
 @Module({
   imports: [
@@ -13,6 +16,11 @@ import { HealthModule } from './health/health.module'
       driver: MercuriusDriver,
     }),
     HealthModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
