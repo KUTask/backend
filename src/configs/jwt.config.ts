e3fea4be2d1d3decfc758b9578409031aa06f09c
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-05-17 11:38:23 +0700

    Add Injectable

--- src/configs/jwt.config.ts
+++ src/configs/jwt.config.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt'
 import { EnviormentVariables } from '.'
 
+@Injectable()
 export class JwtConfigService implements JwtOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
@@ -13,6 +15,9 @@ export class JwtConfigService implements JwtOptionsFactory {
       publicKey: this.configService.get<string>(
         EnviormentVariables.JWT_PUBLIC_KEY,
       ),
+      signOptions: {
+        algorithm: 'ES256',
+      },
     }
   }
 }
--- src/configs/typegoose.config.ts
+++ src/configs/typegoose.config.ts
@@ -2,9 +2,11 @@ import {
   TypegooseModuleOptions,
   TypegooseOptionsFactory,
 } from '@hirasawa_au/nestjs-typegoose'
+import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { EnviormentVariables } from '.'
 
+@Injectable()
 export class TypegooseConfig implements TypegooseOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
