b59486adbcd7b39da1390731a67c4eaface89804
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-05-17 22:27:53 +0700

    Remove unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,9 @@ import { AppService } from './app.service'
 import { GqlConfigService } from './configs/gql.config'
 import { HealthModule } from './health/health.module'
 import { ConfigModule } from '@nestjs/config'
-import { UserModule } from './user/user.module'
 import configuration from './configs'
 import { TypegooseModule } from '@hirasawa_au/nestjs-typegoose'
 import { TypegooseConfig } from './configs/typegoose.config'
-import { RefreshTokenModule } from './refresh-token/refresh-token.module'
 
 @Module({
   imports: [
@@ -23,11 +21,9 @@ import { RefreshTokenModule } from './refresh-token/refresh-token.module'
       isGlobal: true,
       load: [configuration],
     }),
-    UserModule,
     TypegooseModule.forRootAsync({
       useClass: TypegooseConfig,
     }),
-    RefreshTokenModule,
   ],
   controllers: [AppController],
   providers: [AppService],
