2123882b4894034cc2489f1fda8a3ae4bef2b344
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-05-19 14:11:44 +0700

    feat: User can update displayname

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,4 @@
-import { Directive, Mutation, Resolver, Query } from '@nestjs/graphql'
+import { Directive, Mutation, Resolver, Query, Args } from '@nestjs/graphql'
 import { DecodedIdToken } from 'firebase-admin/auth'
 import { User } from './user.decorator'
 import { UserType } from './user.model'
@@ -14,15 +14,28 @@ export class UserResolver {
     return this.userService.find()
   }
 
-  @Mutation(() => UserType, { name: 'createUser' })
+  @Mutation(() => UserType, { name: 'userCreateUser' })
   @Directive('@auth')
-  async createUser(@User() user: Pick<DecodedIdToken, 'uid' | 'name'>) {
+  async createUser(
+    @User() user: Pick<DecodedIdToken, 'uid' | 'name'>,
+    @Args({ type: () => String, nullable: true }) displayName?: string,
+  ) {
+    displayName = displayName ?? user.name
     const hasUser = await this.userService.hasUser(user.uid)
 
     if (hasUser) {
-      return this.userService.findByUid(user.uid)
+      return this.userService.findById(user.uid)
     }
 
-    return this.userService.create(user.uid, user.name)
+    return this.userService.create(user.uid, displayName)
+  }
+
+  @Mutation(() => UserType, { name: 'userUpdateDisplayName' })
+  @Directive('@auth')
+  async updateDisplayName(
+    @User() user: Pick<DecodedIdToken, 'uid'>,
+    @Args({ type: () => String, name: 'displayName' }) displayName: string,
+  ) {
+    return this.userService.updateDisplayName(user.uid, displayName)
   }
 }
