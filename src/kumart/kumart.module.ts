0b3647b8e0a5a825164b28c77676e10b843d3733
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-05-20 02:21:29 +0700

    Remove unused module

--- src/kumart/kumart.module.ts
+++ src/kumart/kumart.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
 import { KumartService } from './kumart.service'
 import { KumartResolver } from './kumart.resolver'
-import { TypegooseModule } from '@hirasawa_au/nestjs-typegoose'
-import { SectionModel } from 'src/models/section.model'
 
 @Module({
   providers: [KumartService, KumartResolver],
